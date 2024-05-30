210c02064337f21b12daf060ab75ce14f571087c
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2024-05-30 16:03:15 +0300

    коммит для максима

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import json
-
 import schedule
 import time
 import bs4
 import datetime
-from dop import keep_alive
 import requests
 from telegram import ReplyKeyboardMarkup, Update, KeyboardButton
 from telegram.ext import MessageHandler, filters, Application, ContextTypes
 from telegram.ext import CommandHandler
 import threading
 
+# Глобальные переменные и настройки
 starti = True
-w = open('settings.txt')
-bogdan_snils = '150-499-330-68'
-andr_snils = '150-862-479-69'
-v = open('vuz.json', encoding='UTF-32')
-vuz = json.load(v)
+bogdan_snils = '150-499-330-68'  #снилс 1
+andr_snils = '150-862-479-69' # снилс 2
+with open('vuz.json', encoding='UTF-32') as v:
+    vuz = json.load(v)
 
-# TOKEN = '5342995443:AAEBqyRLrd5AmHEEhCNLyfHVy3td3Qvw-Ec'  # токен бота
-TOKEN = '6380031031:AAFkhpubZvXUjRVFlWvn6RPugamNppbr4X8'
-markup0 = ReplyKeyboardMarkup([[KeyboardButton('Обновить'), KeyboardButton('Запрос')], [KeyboardButton('Общага')]], one_time_keyboard=False, resize_keyboard=True)
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'  # Замените на ваш токен бота
+markup0 = ReplyKeyboardMarkup([
+    [KeyboardButton('Обновить'), KeyboardButton('Запросить')],
+    [KeyboardButton('Общага')]
+], one_time_keyboard=False, resize_keyboard=True)
+
 last_t = 'Неизвестно'
 it = 0
 last = {}
 ok = False
 
 
+# Функция для проверки списков абитуриентов МИРЭА
 def check_mirea(v, napr):
     global last_t, ok
     link = f'https://priem.mirea.ru/accepted-entrants-list/personal_code_rating.php?competition={vuz[v][napr][0]}'
@@ -40,13 +42,15 @@ def check_mirea(v, napr):
         ok = False
         last_t = last_time
         data = beaLink.find('div', {'class': 'names'}).find('table', {'class': 'namesTable'}).findAll('tr', {})
+
         real_k, kon_all = 0, 0
         b_real_k, b_kon_all = -1, -1
         a_real_k, a_kon_all = -1, -1
         i = 0
+
         for inf in data[1:]:
             fio, d1, d2 = inf.contents[1].get_text(), inf.contents[3].get_text(), inf.contents[4].get_text()
-            if d2 == 'да' or fio in [bogdan_snils, andr_snils]:
+            if d2 == 'да' or  fio in[bogdan_snils, andr_snils]:
                 i += 1
             if fio == bogdan_snils:
                 b_real_k = i
@@ -54,19 +58,22 @@ def check_mirea(v, napr):
                 a_real_k = i
                 break
 
-
         return b_real_k, a_real_k, vuz[v][napr][1]
     except requests.HTTPError as http_error:
         print('Ошибка доступа')
         return -1
 
+
+# Функция для отправки сообщения через Telegram
 def sendMessage(id, text, token):
-    zap = f'''https://api.telegram.org/bot{token}/sendMessage'''
+    zap = f'https://api.telegram.org/bot{token}/sendMessage'
     params = {'chat_id': id, 'text': text}
     return requests.get(zap, params=params).json()
 
+
+# Функция для проверки претендентов на общежитие
 def check_ob():
-    text = [i [:-1]for i in open('mi.txt').readlines()]
+    text = [i.strip() for i in open('mi.txt').readlines()]
     cou = 0
     for q, w in enumerate(text):
         link = f'https://priem.mirea.ru/accepted-entrants-list/personal_code_rating.php?competition={w}'
@@ -79,21 +86,21 @@ def check_ob():
             print(e)
             continue
         for inf in data[1:]:
-            # print(inf.contents)
             if len(inf.contents) != 14:
                 continue
-            if (int(inf.contents[11].get_text()) == 0 or int(inf.contents[11].get_text()) >= 259) and inf.contents[4].get_text() == 'да' and not('отказано' in inf.contents[6].get_text() or 'не' in inf.contents[6].get_text()):
+            if (int(inf.contents[11].get_text()) == 0
+            or int(inf.contents[11].get_text()) >= 259) and inf.contents[4].get_text() == 'да' and not ('отказано' in inf.contents[6].get_text() or 'не' in inf.contents[6].get_text()):
                 cou += 1
             if int(inf.contents[11].get_text()) < 259:
                 break
         print(q, cou)
     return cou
 
+
+# Периодическая задача для проверки списков МИРЭА и уведомления пользователей
 def job():
     date = datetime.datetime.today()
     date += datetime.timedelta(hours=3)
-    # itog = [['Основные положения', ['Протокол ', 'сделан', date.strftime('%d/%m/%Y %H:%M:%S')], [
-    #     'для всех направлений указанных в файле система выводит крайнее место. если человек вылетел из списка, то функция выводит кол-во бюджетных мест']]]
     itog = []
     i = 2
     for v, bibl in vuz.items():
@@ -104,25 +111,17 @@ def job():
             data.append([napr, *check_mirea(v, napr)])
             i += 1
         itog.append([v, *data])
-    # workbook = xlsxwriter.Workbook('Таблица_Excel_БД.xlsx')
-    # for sheet in itog:
-    #     name, *stroki = sheet
-    #
-    #     worksheet = workbook.add_worksheet(name)
-    #     for row, stroka in enumerate(stroki):
-    #         for i in range(len(stroka)):
-    #             worksheet.write(row, i, (stroka[i]))
-    # workbook.close()
+
     global ok, last
     last = itog
     ok = True
     print(date)
+
     global it, starti
     if it != last_t and not starti:
-        a = open('settings.txt')
-        for k in a.readlines():
-            sendMessage(k, 'Внимание, списки обновлены', TOKEN)
-
+        with open('settings.txt') as a:
+            for k in a.readlines():
+                sendMessage(k, 'Внимание, списки обновлены', TOKEN)
 
     it = last_t
     starti = False
@@ -131,28 +130,30 @@ def job():
 job()
 
 
+# Обработчик сообщений Telegram
 async def get(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text == 'Обновить':
         await update.message.reply_text(f'Обработка, прошлый список\n{last_t}', reply_markup=markup0)
         job()
         await update.message.reply_text(f'Обработка завершена\n{last_t}', reply_markup=markup0)
     elif update.message.text == 'Запросить':
-        print(111, last)
         for i in last[0][1:]:
-            sendMessage(update.effective_user.id, f'Направление {i[0]} \nБогдан {i[1]}\nАндрей {i[2]}\n из {i[3]}!', TOKEN)
+            sendMessage(update.effective_user.id, f'Направление {i[0]} \nБогдан {i[1]}\nАндрей {i[2]}\n из {i[3]}!',
+                        TOKEN)
     elif update.message.text == 'Общага':
-        await update.message.reply_text(f'Обработка завершена\n Кол-во людей претендующих на общежитие c баллом >= 259 = {check_ob()}', reply_markup=markup0)
+        await update.message.reply_text(
+            f'Обработка завершена\n Кол-во людей претендующих на общежитие с баллом >= 259 = {check_ob()}',
+            reply_markup=markup0)
 
 
-
-
+# Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    a = open('settings.txt', 'w')
-    a.write(str(update.effective_user.id))
-    print(555)
+    with open('settings.txt', 'w') as a:
+        a.write(str(update.effective_user.id))
     await update.message.reply_text(f'Регистрация пройдена', reply_markup=markup0)
 
 
+# Функция для запуска периодической задачи в отдельном потоке
 def threat():
     while True:
         schedule.run_pending()
@@ -164,9 +165,5 @@ if __name__ == '__main__':
     application = Application.builder().token(TOKEN).build()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get))
     application.add_handler(CommandHandler("start", start))
-    keep_alive()
     threading.Thread(target=threat).start()
     application.run_polling()
-
-
-
